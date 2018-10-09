78cabb0d3c0b162e78e33f4a0cfda9be0bdad5c1
jofas <jonas@fc-web.de>
2018-10-09 15:29:52 +0200

    removal of the old config files in ~ before copying the new ones

--- unpack.sh
+++ unpack.sh
@@ -1,5 +1,10 @@
 tar -zxvf config.tar.gz
 
+# sudo since some files and directories are read only
+sudo rm -r ~/.vim
+rm ~/.vimrc
+rm ~/.tmux.conf
+
 cp -l -r config/vim ~/.vim
 cp -l config/vimrc ~/.vimrc
 cp -l config/tmux.conf ~/.tmux.conf
